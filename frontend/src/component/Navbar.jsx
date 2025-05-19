c85d952b6b4dfd760ebd60a4ccb60e7d8aaaa474
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-20 01:43:59 +0530

    Added ShoppingCartIcon, Avtar from materal UI,Badge to ShoppingCartIcon

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,6 +1,9 @@
-import { IconButton } from '@mui/material'
+import { Badge, IconButton } from '@mui/material'
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import { pink } from '@mui/material/colors';
+import { Avatar } from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
  export const Navbar = () => {
@@ -9,17 +12,27 @@ import SearchIcon from '@mui/icons-material/Search';
       
             <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
                 <li className='logo font-semibold text-gray-300 text-2xl'>
-
+                  San Food
                 </li>
             </div>
             <div className='flex items-center space-x-2 lg:space-x-10'>
               <div className=''>
                 <IconButton>
-                  <SearchIcon sx={{fontSize:"1.5rem"}}/>
-                  
+                  <SearchIcon sx={{fontSize:"1.5rem"}}/>  
                 </IconButton>
-
               </div>
+              <div className=''>
+                <Avatar sx={{bgcolor:"white",color:pink.A400}}>C</Avatar>
+              </div>
+              <div>
+                <IconButton>
+                    <Badge color='secondary' badgeContent = {3}>
+                         <ShoppingCartIcon sx={{fontSize:"1.5rem"}}/>
+                    </Badge>  
+                </IconButton>
+              </div>
+
+
 
             </div>
             
