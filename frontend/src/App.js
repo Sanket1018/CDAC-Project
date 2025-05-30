15c40dcb3c1c41d7eec56f35d77b859ddacf21a8
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-30 17:41:17 +0530

    Added restaurant details,food types,food category

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { CssBaseline, dividerClasses, ThemeProvider } from '@mui/material';
 import { darkTheme } from './Theme/DarkTheme';
 import Home from './component/Home/Home.jsx'
+import RestaurantDetails from './component/Restaurant/RestaurantDetails.jsx';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
 
 
   <Navbar/>
-        <Home/>
+        {/* <Home/> */}
+        <RestaurantDetails/>
       </ThemeProvider>
   );
 }
